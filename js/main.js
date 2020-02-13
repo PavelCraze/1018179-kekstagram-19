f94061638997c8e1dfdf9c805d23daac9f969e26
PavelCraze <54514757+PavelCraze@users.noreply.github.com>
2020-02-14 02:10:46 +0300

    123

--- js/main.js
+++ js/main.js
@@ -10,15 +10,6 @@ var COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-var DESCRIPTIONS = [
-  'Тестим новую камеру!',
-  'Затусили с друзьями на море',
-  'Как же круто тут кормят',
-  'Отдыхаем...',
-  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  'Вот это тачка!'
-];
-
 var LIKES = {
   MIN: 15,
   MAX: 200
@@ -37,37 +28,16 @@ for (var i = 0; i < 25; i++) {
   photos[i] = {
     url: 'photos/' + (i + 1) + '.jpg',
     likes: getRandomPoint(LIKES.MIN, LIKES.MAX),
-    comments: getRandomElement(COMMENTS),
-    description: getRandomElement(DESCRIPTIONS),
+    comments: getRandomElement(COMMENTS)
   };
 }
 
-var makeElement = function (tagName, className, text) {
-  var element = document.createElement(tagName);
-  element.classList.add(className);
-  if (text) {
-    element.textContent = text;
-  }
-  return element;
-};
+var cards = document.querySelector('.pictures');
 
-var createCard = function (photo) {
-  var photoItem = makeElement('a', 'picture');
+var template = document.querySelector('#picture').content.querySelector('.picture');
 
-  var picture = makeElement('img', 'picture__img');
-  picture.src = photo.imgUrl;
-  picture.alt = photo.text;
-  photoItem.appendChild(picture);
-
-  return photoItem;
-};
-
-var cards = document.querySelectorAll('.pictures');
-
-var template = document.querySelector('#picture').content.querySelector('a');
-
-for (var j = 0; j < 25; j++) {
+for (var j = 0; j < photos.length; j++) {
   var element = template.cloneNode(true);
-  element.children[0].textContent = j;
-  cards[1].appendChild(element);
+  template.querySelector('img').src = photos[j].url;
+  cards.appendChild(element);
 }
